a945f66cea9532e86c093bfb0f05fc9508ddb4a9
Glykeria Stravodimou <strglykeria@gmail.com>
2021-02-11 21:52:27 +0000

    added current temp

--- src/interface.js
+++ src/interface.js
@@ -27,8 +27,13 @@ $(document).ready(function() {
     $('#status').text('on');
   })
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temp').text(data.main.temp);
+  })
+
   function UpdateTemperature(){
     $('#temperature').text(thermostat.temp());
     $('#temperature').attr('class', thermostat.energyUsage());
   }
+
 })
